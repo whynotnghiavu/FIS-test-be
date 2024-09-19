f1538be496d8bd4a8faa518fa84dbfee2e540203
whynotnghiavu <176358444+whynotnghiavu@users.noreply.github.com>
2024-09-19 16:43:20 +0700

    GetUser

--- contents/code/python/app/routers/user.py
+++ contents/code/python/app/routers/user.py
@@ -8,8 +8,12 @@ from ..database.get_db import get_db
 
 from ..schemas import user as _schemas_user
 from ..services import user as _services_user
-from ..services.role_checker import RoleChecker
 
+
+from ..services.auth import validate_token
+
+from ..services.role_checker import RoleChecker
+from ..services.get_user import GetUser
 
 router = APIRouter()
 
@@ -22,6 +26,11 @@ def register(user: _schemas_user.UserRegister, db: Session = Depends(get_db)):
 @router.post('/login')
 def login(user: _schemas_user.UserLogin, db: Session = Depends(get_db)):
     return _services_user.login(user, db)
+
+
+@router.post('/me')
+def me(user: Annotated[str, Depends(GetUser())]):
+    return user
 
 
 @router.get("/admin")
