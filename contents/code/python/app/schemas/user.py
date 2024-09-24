e8def8cb92b133e67160f5d8a87c809febdb4145
whynotnghiavu <176358444+whynotnghiavu@users.noreply.github.com>
2024-09-24 11:26:02 +0700

    format schema user

--- contents/code/python/app/schemas/user.py
+++ contents/code/python/app/schemas/user.py
@@ -5,56 +5,48 @@ from pydantic import BaseModel, field_validator
 from ..models import Role
 
 
-# class UserBase(BaseModel):
-#     email: str
-#     password: str
-
-#     @field_validator("email")
-#     def email_validator(cls, email):
-#         if email == "":
-#             raise ValueError("Email must not be empty")
-
-#         email_regex = re.compile(r'^[\w\.-]+@([\w-]+\.)+[\w-]{2,4}$')
-#         if not email_regex.match(email):
-#             raise ValueError("Invalid email format")
-
-#         return email
-
-#     @field_validator("password")
-#     def password_validator(cls, password):
-#         if len(password) < 8:
-#             raise ValueError("Password length must be greater than 8 characters")
-
-#         password_regex = re.compile(r'^(?!.*[_;*\'"`])(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%?&])[A-Za-z\d@$!%?&]{8,}$')
-#         if not password_regex.match(password):
-#             raise ValueError("Invalid password format")
-
-#         return password
-
-
-class UserRegister(BaseModel):
+class UserBase(BaseModel):
     email: str
     password: str
+
+    @field_validator("email")
+    def email_validator(cls, email):
+        if email == "":
+            raise ValueError("Email must not be empty")
+
+        email_regex = re.compile(r'^[\w\.-]+@([\w-]+\.)+[\w-]{2,4}$')
+        if not email_regex.match(email):
+            raise ValueError("Invalid email format")
+
+        return email
+
+    @field_validator("password")
+    def password_validator(cls, password):
+        if len(password) < 8:
+            raise ValueError("Password length must be greater than 8 characters")
+
+        password_regex = re.compile(r'^(?!.*[_;*\'"`])(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%?&])[A-Za-z\d@$!%?&]{8,}$')
+        if not password_regex.match(password):
+            raise ValueError("Invalid password format")
+
+        return password
+
+
+class UserRegister(UserBase):
     role: Role
 
 
-class UserLogin(BaseModel):
-    email: str
-    password: str
+class UserLogin(UserBase):
+    pass
 
-class User(BaseModel):
+
+class User(UserBase):
     id: int
-    email: str
     role: Role
- 
 
-# class User(UserBase):
-#     id: int
-#     role: Role
-
-#     class Config:
-#         # orm_mode = True
-#         from_attributes = True
+    class Config:
+        # orm_mode = True
+        from_attributes = True
 
 
 class JWTUser(User):
@@ -62,5 +54,3 @@ class JWTUser(User):
     role: Role
     jwt_expire: str
     otp_expire: str
-
- 
