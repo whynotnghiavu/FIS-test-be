98352184ec0d1b60ad757903553b3918dcc62ff0
whynotnghiavu <176358444+whynotnghiavu@users.noreply.github.com>
2024-09-27 01:42:40 +0700

    update 1 file, move 1 file and delete 96 files

--- contents/code/python/app/database/create_db.py
+++
@@ -1,8 +0,0 @@
-from ..logger import logger
-from ..models import Base
-from .engine import engine
-
-
-def create_db():
-    logger.warning("Creating database...")
-    Base.metadata.create_all(bind=engine)
--- contents/code/python/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-python check_alembic_db.py
-alembic upgrade head
-
-python main.py
