e730541c2cd393ddb1abfd67af1348e06a799187
whynotnghiavu <176358444+whynotnghiavu@users.noreply.github.com>
2024-09-27 01:50:13 +0700

    update App.js

--- contents/code/frontend/src/App.js
+++ contents/code/frontend/src/App.js
@@ -1,41 +1,18 @@
-import Posts from './Posts.js';
-
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:8000/api')
+      .then(response => response.json())
+      .then(data => setMessage(data.message));
+  }, []);
+
   return (
-   <>
-
-{/* Thanh điều hướng */}
-<nav className="navbar navbar-expand-md bg-dark navbar-dark">
-<a className="navbar-brand" href="/"><span>📖</span></a> 
-<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-<span className="navbar-toggler-icon"></span>
-</button>
-<div className="collapse navbar-collapse" id="collapsibleNavbar">
-
-<ul className="navbar-nav ml-auto">
-
-<li className="nav-item">
-<a className="nav-link" href="/">Register</a>
-</li>
-<li className="nav-item">
-<a className="nav-link" href="/">Login</a>
-</li> 
-</ul>
-</div>  
-</nav>
-
-
-
-<div className="col-sm-4">
-<h3>Posts</h3>
-<Posts  />
-</div>
- 
-
-
-
-   </>
+    <div>
+      <h1>Message from FastAPI: {message}</h1>
+    </div>
   );
 }
 
