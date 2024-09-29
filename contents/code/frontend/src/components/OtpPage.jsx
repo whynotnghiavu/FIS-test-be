54e96fe94e4cf311a984c56ef52d6d2720699824
whynotnghiavu <176358444+whynotnghiavu@users.noreply.github.com>
2024-09-30 06:26:29 +0700

    delete 25 files

--- contents/code/frontend/src/components/OtpPage.jsx
+++
@@ -1,7 +0,0 @@
-const OtpPage = () => { 
-  return ( 
-      <h1>Otp Page</h1> 
-  );
-};
-
-export default OtpPage;
--- contents/code/frontend/src/components/zzzzzzzzzzz.jsx
+++
@@ -1,7 +0,0 @@
-const ZZZZZZZZZZZZZPage = () => { 
-  return ( 
-      <h1>ZZZZZZZZZZZZZ Page</h1> 
-  );
-};
-
-export default ZZZZZZZZZZZZZPage;
